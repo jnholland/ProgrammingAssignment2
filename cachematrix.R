9bec7b268051782167c6befe9f9a3662c7c60aeb
jnholland <jnhollandiii@gmail.com>
2016-06-09 14:42:54 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
+## this function creates a matrix object that can cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     inverse <- NULL
+     set <- function(y) {
+          x <<- y
+          inv <<- NULL
+     }
+     get <- function() x
+     set_inverse <- function(inverse) inverse <<- inverse
+     get_inverse <- function() inverse
+     list(set = set,
+          get = get,
+          set_inverse = set_inverse,
+          get_inverse = get_inverse)
 }
 
 
 ## Write a short comment describing this function
+##  this function calculates the inverse of the matrix created above, but if the inverse has already been created then R gets this inverse from the cache. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     inverse <- x$get_inverse()
+     if (!is.null(inverse)) {
+          message("getting cached inverse")
+          return(inverse)
+     }
+     inv_mat <- x$get()
+     inverse <- solve(inv_mat, ...)
+     x$set_inverse(inverse)
+     inverse
 }
